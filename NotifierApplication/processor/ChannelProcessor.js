d5ffbef6b4a08bfe8294757bce688855d2e6e938
Cha <chaliang.chu@amis.nl>
2021-08-31 20:00:07 +0200

    WIP

--- NotifierApplication/processor/ChannelProcessor.js
+++ NotifierApplication/processor/ChannelProcessor.js
@@ -7,3 +7,23 @@ var ChannelProcessor = function (channel, notificationService) {
 module.exports = {
     ChannelProcessor
 }
+
+
+// class ChannelProcessor {
+
+//     instant = Instant.EPOCH;
+//     notificationService = 1;
+//     channel = new Channel();
+
+//     ChannelProcessor(channel, notificationService) {
+//         this.channel = channel;
+//         this.notificationService = notificationService;
+//     }
+
+//     run() {
+//         var notificationList = notificationService.getNotifications(channel.getChannelId(), instant);
+//         var optional = notificationList.stream().filter(notification => notification.getCreatedAt() != null).map(Notification => Notification.getCreatedAt).max(Instant => Instant.compareTo);
+//         if (optional.isPresent()) this.instant = optional.get();
+//         notificationsList.forEach(notification => System.out.println(notification.getMessage()));
+//     }
+// }
---
+++ NotifierApplication/web/SubscriptionController.js
@@ -0,0 +1,32 @@
+// package nl.amis.web;
+
+// import nl.amis.data.jpa.domain.Subscription;
+// import nl.amis.data.jpa.service.SubscriptionRepository;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import java.util.List;
+
+// @RestController
+// public class SubscriptionController {
+
+// 	private final SubscriptionRepository subscriptionRepository;
+
+// 	public SubscriptionController(SubscriptionRepository subscriptionRepository) {
+// 		this.subscriptionRepository = subscriptionRepository;
+// 	}
+
+// 	@RequestMapping("/")
+// 	@ResponseBody
+// 	public String toDo() {
+// 		return "Not implemented yet";
+// 	}
+
+// 	@GetMapping("/subscriptions")
+// 	Iterable<Subscription> all() {
+// 		return subscriptionRepository.findAll();
+// 	}
+
+// }
