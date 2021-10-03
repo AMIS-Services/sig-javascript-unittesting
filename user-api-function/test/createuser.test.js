6b5528e66a0b54216320ab2a6d418ee06e5f2005
Henk Jan van Wijk <henkjan.van.wijk@amis.nl>
2021-10-04 00:00:42 +0200

    Update unittest

--- user-api-function/test/createuser.test.js
+++ user-api-function/test/createuser.test.js
@@ -5,9 +5,18 @@ jest.mock('../common/database', () => ({
   ...(jest.requireActual('../common/database')),
   executeTransaction: jest.fn()
 }))
+const validation = require('../createUser/validation')
+jest.mock('../createUser/validation', () => ({
+  ...(jest.requireActual('../createUser/validation')),
+  validateRequest: jest.fn()
+}))
 
 describe('Create a user', () => {
-  it('should return an id when the call is succesful', async () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should return status 201 with id when succesful', async () => {
     const expectedResponse = { id: 1 }
     const user = {
       firstname: 'Jan',
@@ -22,7 +31,8 @@ describe('Create a user', () => {
       date_created: new Date().toISOString(),
       date_modified: new Date().toISOString()
     }
-    db.executeTransaction.mockReturnValue({})
+    validation.validateRequest.mockReturnValue(true)
+    db.executeTransaction.mockReturnValue(expectedResponse)
     const executeTransactionCall = jest.spyOn(db, 'executeTransaction')
     const request = {
       body: user
@@ -33,5 +43,63 @@ describe('Create a user', () => {
     expect(context.res.body).toEqual(expectedResponse)
     expect(executeTransactionCall).toHaveBeenCalled()
     expect(executeTransactionCall).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return status 400 when mandatory fields are missing', async () => {
+    const expectedResponse = {
+      error: 'Missing mandatory fields'
+    }
+    const user = {
+      lastname: 'Janszen',
+      prefix: 'string',
+      street: 'Dorpstraat',
+      houseno: 14,
+      postalcode: '9888AC',
+      city: 'Ons Dorp',
+      country: 'NL',
+      email1: 'jan.janszen@example.com',
+      date_created: new Date().toISOString(),
+      date_modified: new Date().toISOString()
+    }
+    validation.validateRequest.mockReturnValue(false)
+    const executeTransactionCall = jest.spyOn(db, 'executeTransaction')
+    const validateRequestCall = jest.spyOn(validation, 'validateRequest')
+    const request = {
+      body: user
+    }
+    const context = new ContextLogger()
+    await createUser(context, request)
+    expect(context.res.status).toBe(400)
+    expect(context.res.body).toEqual(expectedResponse)
+    expect(executeTransactionCall).not.toHaveBeenCalled()
+    expect(validateRequestCall).toHaveBeenCalled()
+    expect(validateRequestCall).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return status 409 when existing record is found', async () => {
+    const expectedResponse = {
+      error: 'Missing mandatory fields'
+    }
+    const user = {
+      firstname: 'Jan',
+      lastname: 'Janszen',
+      prefix: 'string',
+      street: 'Dorpstraat',
+      houseno: 14,
+      postalcode: '9888AC',
+      city: 'Ons Dorp',
+      country: 'NL',
+      email1: 'jan.janszen@example.com',
+      date_created: new Date().toISOString(),
+      date_modified: new Date().toISOString()
+    }
+    validation.validateRequest.mockReturnValue(true)
+    const request = {
+      body: user
+    }
+    const context = new ContextLogger()
+    await createUser(context, request)
+    expect(context.res.status).toBe(400)
+    expect(context.res.body).toEqual(expectedResponse)
   })
 })
